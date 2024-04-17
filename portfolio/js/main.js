90c6c883f223ebead055558cf4ef03471b285d3b
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-04-17 17:33:46 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -11,4 +11,30 @@ $(document).ready(function(){
         mousewheel: true,
         freeMode: true,
     });
+    $('.index .cnt li').not('.num03, .num04').on('mouseenter', function(){
+        $('.index .cnt li').removeClass('on')
+        $(this).addClass('on')
+    })
+    /*$('.index .cnt li').on('mouseleave', function(){
+        $('.index .cnt li').removeClass('on')
+    })*/
+    $('.header .gnb_wrap ul li[data-link="main"]').on('click', function(){
+        swiper.slideTo(0, 500)
+    });
+    $('.header .gnb_wrap ul li[data-link="intro"]').on('click', function(){
+        swiper.slideTo(1, 500)
+    });
+    $('.header .gnb_wrap ul li[data-link="project"]').on('click', function(){
+        swiper.slideTo(2, 500)
+    });
+    $('.header .gnb_wrap ul li[data-link="contact"]').on('click', function(){
+        swiper.slideTo(5, 500)
+    });
+    $('.index ul li.kps').on('click', function(){
+        swiper.slideTo(3, 500)
+    });
+    $('.index ul li.hospital').on('click', function(){
+        swiper.slideTo(4, 500)
+    });
+
 })
