31b7c7aec19b87cb32a57aa1a7317bac162b8ed5
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-04-24 18:09:10 +0900

    .

--- pusan_uni/js/common.js
+++ pusan_uni/js/common.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+    $('.header .gnb .depth01 > li').on('mouseenter', function(){
+        $('.header').addClass('menu_over')
+        $('.header .gnb .depth01 > li').removeClass('on')
+        $(this).addClass('on')
+    })
+    $('.header .gnb .depth01 > li').on('mouseleave', function(){
+        $('.header').removeClass('menu_over')
+        $('.header .gnb .depth01 > li').removeClass('on')
+    })
+    $('.header .gnb .depth02 > li').on('mouseenter', function(){
+        $('.header .gnb .depth02 > li').removeClass('on')
+        $(this).addClass('on')
+    })
     $('.header_top .tnb .service .open').on('click', function(){
         $('.header_top .tnb .service').addClass('on')
     })
