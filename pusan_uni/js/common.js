acf2877062d7ee3fb2d337266d8970ccc4be95e0
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-04-24 13:25:16 +0900

    .

--- pusan_uni/js/common.js
+++ pusan_uni/js/common.js
@@ -5,4 +5,10 @@ $(document).ready(function(){
     $('.header_top .tnb .service .close').on('click', function(){
         $('.header_top .tnb .service').removeClass('on')
     })
+    $('.header_top .tnb .lang .open').on('click', function(){
+        $('.header_top .tnb .lang').addClass('on')
+    })
+    $('.header_top .tnb .lang .close').on('click', function(){
+        $('.header_top .tnb .lang').removeClass('on')
+    })
 })
