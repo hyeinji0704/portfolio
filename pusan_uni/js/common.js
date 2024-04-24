55d696bc51cc11afadb315623a8219534f21450e
hyeinji0704 <157083486+hyeinji0704@users.noreply.github.com>
2024-04-25 02:23:08 +0900

    .

--- pusan_uni/js/common.js
+++ pusan_uni/js/common.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+    let scrolling 
+    function scroll_chk(){
+        scrolling = $(window).scrollTop()
+        if(scrolling > 0){//조금이라도 스크롤 함
+            $('.header').addClass('fixed')
+        }else{//상단에 있는 경우 -- 스크롤 안한 경우 또는 다시 올라간 경우
+            $('.header').removeClass('fixed')
+        }
+    }
+    scroll_chk() //문서 로드되었을 때 1번 실행
+    $(window).scroll(function(){
+        scroll_chk()
+    })
+
+
+    
     $('.header .gnb .depth01 > li').on('mouseenter', function(){
         $('.header').addClass('menu_over')
         $('.header .gnb .depth01 > li').removeClass('on')
